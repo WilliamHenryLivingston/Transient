d5b0ea20a63fd204ec33f681e96544397b6dae6f
robinsax <robinsaxifrage@live.ca>
2023-02-22 14:58:46 -0800

    Switch to C++ project

---
+++ Source/Transient/TransientGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TransientGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRANSIENT_API ATransientGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
