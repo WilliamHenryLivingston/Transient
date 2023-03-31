867d82730b66db11c0ede072f4557da0f92381cc
robinsax <robin@stonebreakerdesigns.com>
2023-03-31 01:29:41 -0700

    Add lootable containers, multiple ammo types. Rework stamina and energy

---
+++ Source/Transient/StatusUIWidget.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Blueprint/UserWidget.h"
+
+#include "StatusUIWidget.generated.h"
+
+UCLASS()
+class TRANSIENT_API UStatusUIWidget : public UUserWidget {
+	GENERATED_BODY()
+	
+};
